e7e5bdefaf4adb85ceb5b148cb2673bcb8d03eb5
Max Edlund <maxedlund88@gmail.com>
2023-09-23 23:17:02 +0200

    add router and needed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './routes/Home';
+import { About } from './routes/About';
+import { Articles } from './routes/Articles';
+import { Article } from './routes/Article';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <div>
+          <h1>Blog</h1>
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/articles" element={<Articles />} />
+              <Route path="/article/:articleId" element={<Article />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
