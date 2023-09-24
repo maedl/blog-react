af5c540b4ce335990e902e7b1e62b7f60ad6cf22
Max Edlund <maxedlund88@gmail.com>
2023-09-24 13:15:35 +0200

    add content to Articles route

--- src/routes/Articles.tsx
+++ src/routes/Articles.tsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom';
+import { mockArticles } from '../models/IArticle';
+
 export const Articles = () => {
-  return <div>Articles</div>;
+  return (
+    <div className="flex-container">
+      <h1>Articles</h1>
+      <div className="articles-container">
+        {mockArticles.map((article) => (
+          <div className="single-articles-container" key={article.id}>
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <Link to={`/article/${article.id}`}>Read</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
